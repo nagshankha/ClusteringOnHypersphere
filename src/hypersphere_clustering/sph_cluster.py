5def55901a509039ef68768cf89988929621f8c3
Shankha Nag <shankha.nag90@gmail.com>
2025-03-26 21:00:22 +0100

    Created sph_cluster.py file in src folder

---
+++ src/hypersphere_clustering/sph_cluster.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class ClusterInitiation:
+    
+    def __init__(self, n_clusters, cluster_initiation, random_state=None):
+        
+        self.n_clusters = n_clusters
+
+class Clustering(ClusterInitiation):
+    
+    def __init__(self, n_clusters, max_iter=300, cost_tolerance=0.0001, 
+                 cluster_initiation='recommended', random_state=None):
+        pass
+
+
+        
