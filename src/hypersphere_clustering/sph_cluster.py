747ec265b7910c0e1f0e02d46ce61ca318795d6e
Shankha Nag <shankha.nag90@gmail.com>
2025-03-26 21:21:17 +0100

    Defined classes SeedInitiation and Clustering with __init__ method

--- src/hypersphere_clustering/sph_cluster.py
+++ src/hypersphere_clustering/sph_cluster.py
@@ -1,16 +1,22 @@
 import numpy as np
 
-class ClusterInitiation:
+class SeedInitiation:
     
-    def __init__(self, n_clusters, cluster_initiation, random_state=None):
+    def __init__(self, n_clusters, seed_initiation, random_state):
         
         self.n_clusters = n_clusters
 
 class Clustering(ClusterInitiation):
     
     def __init__(self, n_clusters, max_iter=300, cost_tolerance=0.0001, 
-                 cluster_initiation='recommended', random_state=None):
-        pass
+                 seed_initiation='recommended', random_state=None):
+        
+        SeedInitiation.__init__(n_clusters, seed_initiation, random_state)
+        self.max_iter = max_iter
+        self.cost_tolerance = cost_tolerance
+        
+    
+        
 
 
         
