7b439a28a9e49636dd9547d11195e36e51812897
Shankha Nag <shankha.nag90@gmail.com>
2025-03-26 22:55:52 +0100

    Added method 'predict' to class 'Clustering'

--- src/hypersphere_clustering/sph_cluster.py
+++ src/hypersphere_clustering/sph_cluster.py
@@ -27,13 +27,17 @@ class Clustering(SeedInitiation):
         self.n_obs, self.n_features = np.shape(X)
         self.generate_cluster_centers()
         cluster_centers = self.initial_cluster_centers
+        self.cost_per_iter = []
         
         for i in range(self.max_iter):
             d_sq = 1-np.dot(X, cluster_centers.T)
             cluster_assignments = np.argmin(d_sq, axis=1)
             cost = np.sum(np.arg(d_sq, axis=1))
             if cost <= self.cost_tolerance:
+                self.cost_per_iter.extend([cost])
                 break
+            else:
+                self.cost_per_iter.extend([cost])
             for c_id in range(self.n_clusters):
                 cluster_centers[c_id] = np.sum(X[cluster_assignments==c_id], 
                                                axis=1)
@@ -41,6 +45,13 @@ class Clustering(SeedInitiation):
             
         self.cluster_centers = cluster_centers
         self.cluster_assignments = cluster_assignments
+        
+    def predict(self, X):
+        
+        d_sq = 1-np.dot(X, self.cluster_centers.T)
+        cluster_assignments = np.argmin(d_sq, axis=1)
+        
+        return cluster_assignments
     
         
 
