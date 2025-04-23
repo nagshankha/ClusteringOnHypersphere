907f0299ae3c94fa5df9e100009b03d9fe5a8dde
shankha.nag90 <shankha.nag90@gmail.com>
2025-04-23 16:50:33 +0000

    Edited __setattr__ of vectors.py

--- src/CosineSimilarityAnalyses/vectors.py
+++ src/CosineSimilarityAnalyses/vectors.py
@@ -5,7 +5,7 @@ import pandas as pd
 class Vectors:
 
     def __init__(self, x):
-        if isinstance(x, np.ndarray) and x.ndim == 2 and 
+        if isinstance(x, np.ndarray) and (x.ndim == 2) and 
                         np.issubdtype(x.dtype, np.floating):            
             self.data = normalize(x)
             self.n_samples, self.n_features = np.shape(x)
@@ -32,3 +32,30 @@ class Vectors:
         elif len(index) == 1:
             self.data[index[0]] = value)
         
+    def __setattr__(self, name, value):
+
+        if name == 'data':
+            if isinstance(x, np.ndarray) and (x.ndim == 2) and 
+                        np.issubdtype(x.dtype, np.floating) and
+                        np.allclose(np.linalg.norm(x, axis=1), 1):
+                self.__dict__[name] = x
+            else:
+                raise ValueError("data attribute of Vectors instance "+
+                "must be a 2D numpy array with each row being an unit vector.")
+
+        elif name == 'n_samples':
+            if value == np.shape(self.data)[0]:
+                self.__dict__[name] = value
+            else:
+                raise ValueError("n_samples attribute of Vectors instance "+
+                "must be equal to the number of rows in the data attribute.")
+
+        elif name == 'n_features':
+            if value == np.shape(self.data)[1]:
+                self.__dict__[name] = value
+            else:
+                raise ValueError("n_features attribute of Vectors instance "+
+                "must be equal to the number of columns in the data attribute.")
+
+        else:
+            self.__dict__[name] = value
