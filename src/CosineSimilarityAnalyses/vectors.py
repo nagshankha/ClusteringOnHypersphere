7b4883d6b89f55f653f1ec58ec5569660bcc2151
shankha.nag90 <shankha.nag90@gmail.com>
2025-04-23 17:01:46 +0000

    Edited __getitem__ of vectors.py

--- src/CosineSimilarityAnalyses/vectors.py
+++ src/CosineSimilarityAnalyses/vectors.py
@@ -8,29 +8,29 @@ class Vectors:
         if isinstance(x, np.ndarray) and (x.ndim == 2) and 
                         np.issubdtype(x.dtype, np.floating):            
             self.data = normalize(x)
-            self.n_samples, self.n_features = np.shape(x)
         elif isinstance(x, Vectors):
             self.data = x.data
-            self.n_samples = x.n_samples
-            self.n_features = x.n_features
         else:
             raise ValueError("Invalid input type. "+
             "Expected a 2D numpy array of floating-point "+
             "numbers or an instance of Vectors.")
 
+    def distance(self, other:Vectors=None) -> np.ndarray:
+        pass
+
     def __getitem__(self, index):
         if len(index) == 2:
             return self.data[index[0], index[1]]
         elif len(index) == 1:
-            return self.data[index[0]]
+            return Vectors(self.data[index[0]])
         else:
-            raise ValueError("Invalid index. Expected a 1D or 2D index.")
+            raise ValueError("Invalid index. Expected a 1-tuple or 2-tuple")
 
-    def __setitem__(self, index, value):
-        if len(index) == 2:
-            self.data[index[0], index[1]] = value
-        elif len(index) == 1:
-            self.data[index[0]] = value)
+    def __iter__():
+        pass
+
+    def __next__():
+        pass
         
     def __setattr__(self, name, value):
 
@@ -39,6 +39,7 @@ class Vectors:
                         np.issubdtype(x.dtype, np.floating) and
                         np.allclose(np.linalg.norm(x, axis=1), 1):
                 self.__dict__[name] = x
+                self.n_samples, self.n_features = np.shape(x)
             else:
                 raise ValueError("data attribute of Vectors instance "+
                 "must be a 2D numpy array with each row being an unit vector.")
